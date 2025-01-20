566106389699bca8d6fd3ffabbfd47891e3c4f67
Tastac <keyganap@gmail.com>
2025-01-20 14:20:38 -0800

    update

---
+++ scripts/Enemy.gd
@@ -0,0 +1,15 @@
+extends Node
+
+# 0 idle 1 walk 2 searching 3 attack
+@onready var jobs : int = 0
+@onready var Player = $Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#if()
+	pass
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
-@export var starting_direction : Vector2 = Vector2(0, 1)
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready():
-	update_animation_parameters(starting_direction)
+	update_animation_parameters(Vector2(0, 1))
 
 func _physics_process(_delta):
 	# Movement
