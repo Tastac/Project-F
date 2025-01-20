a7611a11d2e8137e23cab194c96978eaf1ffc68a
Tastac <keyganap@gmail.com>
2025-01-19 17:15:55 -0800

    updated project

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,19 +1,35 @@
-extends RigidBody2D
+extends CharacterBody2D
 
-@export var speed = 200
+@export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0, 1)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	update_animation_parameters(starting_direction)
 
-func get_input():
+func _physics_process(_delta):
+	# Movement
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	linear_velocity = input_direction * speed;
+	velocity = input_direction * move_speed
 	
-func _physics_process(delta: float) -> void:
-	self.rotation = 0;
-	get_input()
+	update_animation_parameters(input_direction)
+	
+	move_and_slide()
+	pick_new_state()
+	
+func update_animation_parameters(move_input: Vector2):
+	
+	if(move_input != Vector2.ZERO):
+		animation_tree["parameters/Walk/blend_position"] = move_input 
+		animation_tree["parameters/Idle/blend_position"] = move_input
+		
+
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walk",false)
+	else:
+		state_machine.travel("Idle",false)
+
+var idle_direction: String = "Idle" # Same names as animation
