399a87e2b33b8f312856ae09b21ddeab6deb343c
cannibalBunnii <aurumcometam@protonmail.com>
2025-01-21 22:13:11 -0500

    Bunnycont

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,10 +1,5 @@
 extends Area2D
 
-@onready var timer: Timer = $Timer
-
 func _on_body_entered(body: CharacterBody2D) -> void:
-	print("You have Died")
-	timer.start()
-
-func _on_timer_timeout() -> void:
+	print("You Died!")
 	get_tree().reload_current_scene()
