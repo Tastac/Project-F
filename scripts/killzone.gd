202a3549b05313c3d1071b6087634d0001f2d248
cannibalBunnii <aurumcometam@protonmail.com>
2025-01-22 21:58:45 -0500

    BunnyCont

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
 func _on_body_entered(body: CharacterBody2D) -> void:
+<<<<<<< Updated upstream
 	print("You Died!")
+=======
+	print("You have Died")
+>>>>>>> Stashed changes
 	get_tree().reload_current_scene()
