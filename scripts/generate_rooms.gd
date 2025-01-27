91da28c3600e4df6e5c34af76d6814b5775cf45e
Tastac <keyganap@gmail.com>
2025-01-27 09:47:06 -0800

    Sort of generated room
    I realize that time that allocated is not enough time to make a generated room...

--- scripts/generate_rooms.gd
+++ scripts/generate_rooms.gd
@@ -1,8 +1,13 @@
 extends Node
 
-@export var numbers_rooms = 5
+
+
+@export var number_rooms: int = 10
 
 var random_rooms = RandomNumberGenerator.new()
+
+@export var grid_size: int = 275
+@export var dungeon = {}
 
 #var LoadedRoom: Array[] = 
 
@@ -17,35 +22,56 @@ var random_rooms = RandomNumberGenerator.new()
 	preload("res://scenes/rooms/End01.tscn")
 ]
 
+var room_scene: PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	setupRooms()
+	place_rooms()
 	#pass # Replace with function body.
 
 func setupRooms():
+	var current_pos = Vector2(0, 0)
+	dungeon[current_pos] = true
 	
-	#$Rooms.add_child(StartingRooms[random_rooms.randi_range(0, len(StartingRooms))].instantiate())
-	#var room0 = $Rooms.get_child(0)
-	#Starting Room
-	#$Rooms.add_child(StartingRooms[0].instantiate())
-	#Rooms
-	#for i in numbers_rooms:
-	
-	$Rooms.add_child(StartingRooms[random_rooms.randi_range(0, len(StartingRooms) - 1)].instantiate())
-	$Rooms.add_child(Rooms[0].instantiate())
-	
-	$Rooms.get_child(1).get_child(0)
-	
-	if($Rooms.get_child(0).doors[1] and $Rooms.get_child(1).doors[0]):
-		#random_rooms.randi_range(0, len(Rooms) - 1)].instantiate())
-		$Rooms.get_child(1).position += Vector2(0, 50)
-	
-	#var room1 = 
-	print($Rooms.get_children())
-	#.position += Vector2(10, 10)
-	
-	#$Rooms.add_child()
+	for i in range(number_rooms - 1):
+		var new_pos = get_random_adjacent_position(current_pos)
+		while dungeon.has(new_pos):
+			new_pos = get_random_adjacent_position(current_pos)
+		dungeon[new_pos] = true
+		current_pos = new_pos
 
+func get_random_adjacent_position(position: Vector2) -> Vector2:
+	var dir = [
+		Vector2(0, -1),
+		Vector2(0, 1),
+		Vector2(-1, 0),
+		Vector2(1, 0)
+	]
+	return position + dir[randi() % dir.size()]
+	
+func place_rooms():
+	for position in dungeon.keys():
+		var room_instance = StartingRooms[random_rooms.randi_range(0, StartingRooms.size() - 1)].instantiate()
+		room_instance.position = position * grid_size
+		add_child(room_instance)
+		connect_doors(room_instance, position)
+
+func connect_doors(room: Node2D, pos: Vector2):
+	var dir = {
+		"DoorTop": Vector2(0, -1),
+		"DoorBottom": Vector2(0, 1),
+		"DoorLeft": Vector2(-1, 0),
+		"DoorRight": Vector2(1, 0)
+	}
+	
+	for door_name in dir:
+		var neighbor_position = pos + dir[door_name]
+		if dungeon.has(neighbor_position):
+			continue
+		else:
+			if room.has_node(door_name):
+				room.get_node(door_name).queue_free()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
